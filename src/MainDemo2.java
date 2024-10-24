e56172c7113cd2ca10e647a2c7f8e810d52ffc20
Le Anh Duc <leanhduc2500.cv@gmail.com>
2024-10-24 21:59:14 +0700

    game map level 3

---
+++ src/MainDemo2.java
@@ -0,0 +1,9 @@
+import view.GameFrame2;
+
+import java.awt.*;
+
+public class MainDemo2 {
+    public static void main(String[] args) {
+       new GameFrame2(2);
+    }
+}
---
+++ src/gamecomponent/enviroment/River.java
@@ -0,0 +1,14 @@
+package gamecomponent.enviroment;
+
+import gamecomponent.Position;
+
+import javax.swing.*;
+
+public class River extends Enviroment{
+    public River(Position position) {
+        super(position);
+        this.canPass = false;
+        this.canDestroy = false;
+        this.image = new ImageIcon("images/river.jpg").getImage();
+    }
+}
